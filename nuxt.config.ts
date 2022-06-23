e13747dd367aae8c4b8b0ba19c4920ea3d1c5636
Meysam Kiani <dev.mysm@gmail.com>
2022-06-23 22:44:24 +0430

    chore: remove vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,5 @@
 import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
-  css: [
-    'vuetify/lib/styles/main.sass',
-    'mdi/css/materialdesignicons.min.css',
-  ],
-
   buildModules: ['@pinia/nuxt'],
-  build: {
-    transpile: ['vuetify'],
-  },
-
-  vite: {
-    define: {
-      'process.env.DEBUG': false,
-    }
-  },
 })
