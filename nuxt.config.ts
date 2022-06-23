5980e3ed4512bbf0de3aa2c62085bb2dcc3c391a
Meysam Kiani <dev.mysm@gmail.com>
2022-06-23 19:10:16 +0430

    chore: add vuetify and mdi icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
-  buildModules: ['@pinia/nuxt']
+  css: [
+    'vuetify/lib/styles/main.sass',
+    'mdi/css/materialdesignicons.min.css',
+  ],
+
+  buildModules: ['@pinia/nuxt'],
+  build: {
+    transpile: ['vuetify'],
+  },
+
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    }
+  },
 })
