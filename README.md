5e8b2dbabf4b15fd5d9c0dec3ec39f1939faa0ce
itmysm <itmysm@gmail.com>
2023-01-14 01:41:58 +0330

    ❌ del: remove old dirs/files

--- README.md
+++
@@ -1,2 +0,0 @@
-## Mysm
-this is my personal website that works on your mobile or desktop browser. You can see the live demo [here](https://mysm.ir)
--- stores/error.js
+++
@@ -1,7 +0,0 @@
-import { defineStore } from "pinia";
-
-export const useError = defineStore('errors', {
-  state: () => ({
-    error: { code: 404, message: 'This page could not be found.' },
-  }),
-})
